7da14ec2752e8a9171e6df670510c2ef0ed15bd1
ahuacate <43461046+ahuacate@users.noreply.github.com>
2019-05-30 14:37:14 +0700

    Update iptables.sh

--- openvpn/iptables.sh
+++ openvpn/iptables.sh
@@ -6,21 +6,4 @@ iptables -t mangle -F
 iptables -F
 iptables -X
 
-# Block All
-iptables -P OUTPUT DROP
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
-
-# allow Localhost
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
-
-# Make sure you can communicate with any DHCP server
-iptables -A OUTPUT -d 255.255.255.255 -j ACCEPT
-iptables -A INPUT -s 255.255.255.255 -j ACCEPT
-
-# Make sure that you can communicate within your own network
-iptables -A INPUT -s 192.168.0.0/24 -d 192.168.0.0/24 -j ACCEPT
-iptables -A OUTPUT -s 192.168.0.0/24 -d 192.168.0.0/24 -j ACCEPT
-
 service iptables save
