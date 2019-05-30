42a8488ec8305506721e1abda34977bc86c881ba
ahuacate <43461046+ahuacate@users.noreply.github.com>
2019-05-30 14:36:46 +0700

    Update iptables.sh

--- openvpn/iptables.sh
+++ openvpn/iptables.sh
@@ -23,29 +23,4 @@ iptables -A INPUT -s 255.255.255.255 -j ACCEPT
 iptables -A INPUT -s 192.168.0.0/24 -d 192.168.0.0/24 -j ACCEPT
 iptables -A OUTPUT -s 192.168.0.0/24 -d 192.168.0.0/24 -j ACCEPT
 
-# Allow established sessions to receive traffic:
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-# Allow TUN
-iptables -A INPUT -i tun+ -j ACCEPT
-iptables -A FORWARD -i tun+ -j ACCEPT
-iptables -A FORWARD -o tun+ -j ACCEPT
-iptables -t nat -A POSTROUTING -o tun+ -j MASQUERADE
-iptables -A OUTPUT -o tun+ -j ACCEPT
-
-# allow VPN connection
-#iptables -I OUTPUT 1 -p udp --destination-port 1195 -m comment --comment "Allow VPN connection" -j ACCEPT
-
-# Block All
-iptables -A OUTPUT -j DROP
-iptables -A INPUT -j DROP
-iptables -A FORWARD -j DROP
-
-# Log all dropped packages, debug only.
-#iptables -N logging
-#iptables -A INPUT -j logging
-#iptables -A OUTPUT -j logging
-#iptables -A logging -m limit --limit 2/min -j LOG --log-prefix "IPTables general: " --log-level 7
-#iptables -A logging -j DROP
-
 service iptables save
