49c1c41595ed8a67538fc6cd2e5f03a77822b0b0
ahuacate <43461046+ahuacate@users.noreply.github.com>
2019-07-02 19:43:06 +0700

    Update typhoon_node-setup-01.sh

--- scripts/typhoon_node-setup-01.sh
+++ scripts/typhoon_node-setup-01.sh
@@ -1,3 +1,36 @@
+# Edit Proxmox host file
+echo -e "127.0.0.1 localhost.localdomain localhost
+192.168.1.101 typhoon-01.localdomain.com typhoon-01
+192.168.1.102 typhoon-02.localdomain.com typhoon-02
+192.168.1.103 typhoon-03.localdomain.com typhoon-03
+192.168.1.104 typhoon-04.localdomain.com typhoon-04
+
+# The follow are network machines
+192.168.1.10 cyclone-01 cyclone-01.localdomain.com cyclone-01
+
+# The following lines are desirable for IPv6 capable hosts
+
+::1     ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts"  >  /etc/hosts
+
+# Create a PAM User Group
+pveum groupadd homelab -comment 'Homelab User Group'
+# Add PVEVMAdmin role (fully administer VMs) to group homelab
+pveum aclmod / -group homelab -role PVEVMAdmin
+# Create PAM User
+pveum useradd storm@pam -comment 'User Storm'
+pveum passwd storm@pam
+# Add User to homelab group
+pveum usermod storm@pam -group homelab
+
+cat /home/james/.ssh/id_rsa.pub | ssh [USER]@[SERVER] "cat >> ~/.ssh/authorized_keys"
+
+/etc/pve/storage.cfg
+
 zfspool: typhoon-share
         pool typhoon-share
         content rootdir,images
@@ -9,4 +42,5 @@ nfs: cyclone-01-backup
         server 192.168.1.10
         content backup
         maxfiles 1
-        options vers=3
+        options vers=4
+PVEVMAdmin
