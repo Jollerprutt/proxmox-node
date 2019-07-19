e9bd30ee315c1d0ac964ee7fa3386ab3f4137c33
ahuacate <43461046+ahuacate@users.noreply.github.com>
2019-07-19 12:22:08 +0700

    Update typhoon-0X-Single_NIC-setup-01.sh

--- scripts/typhoon-0X-Single_NIC-setup-01.sh
+++ scripts/typhoon-0X-Single_NIC-setup-01.sh
@@ -17,14 +17,6 @@ echo "This script will also create a new Proxmox username called 'storm'. So hav
 sleep 2.5
 echo ""
 
-read -p "Are you sure? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-fi
-
-
 # Create a New User called 'storm'
 # Create a New PVE User Group
 pveum groupadd homelab -comment 'Homelab User Group'
