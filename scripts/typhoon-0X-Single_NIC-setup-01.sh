58eda01f6633f73131457eaa806b7e0b8b13bc23
ahuacate <43461046+ahuacate@users.noreply.github.com>
2019-07-19 15:23:08 +0700

    Update typhoon-0X-Single_NIC-setup-01.sh

--- scripts/typhoon-0X-Single_NIC-setup-01.sh
+++ scripts/typhoon-0X-Single_NIC-setup-01.sh
@@ -7,9 +7,10 @@
 ###################################################################
 
 # Command to run script 
-# wget https://raw.githubusercontent.com/ahuacate/proxmox-node/master/scripts/typhoon-0X-Single_NIC-setup-01.sh -P /tmp && ./tmp/typhoon-0X-Single_NIC-setup-01.sh; -rf /tmp/typhoon-0X-Single_NIC-setup-01.sh
+# wget https://raw.githubusercontent.com/ahuacate/proxmox-node/master/scripts/typhoon-0X-Single_NIC-setup-01.sh -P /tmp && chmod +x /tmp/typhoon-0X-Single_NIC-setup-01.sh && bash /tmp/typhoon-0X-Single_NIC-setup-01.sh; rm -rf /tmp/typhoon-0X-Single_NIC-setup-01.sh
 
 # Basic Details
+echo "All passwords must have a minimum of 5 characters"
 read -p "Please Enter a password for user storm: " stormpasswd
 
 # Create a New User called 'storm'
