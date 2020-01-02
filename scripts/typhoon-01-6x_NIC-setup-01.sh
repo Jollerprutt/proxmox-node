dc1f58798d2a36ba5a04b1bb8b4bc09c2b478e18
Ahuacate <43461046+ahuacate@users.noreply.github.com>
2020-01-02 12:10:20 +0700

    Update typhoon-01-6x_NIC-setup-01.sh

--- scripts/typhoon-01-6x_NIC-setup-01.sh
+++ scripts/typhoon-01-6x_NIC-setup-01.sh
@@ -58,45 +58,10 @@ pvesm add nfs cyclone-01-photo --path /mnt/pve/cyclone-01-photo --server 192.168
 pvesm add nfs cyclone-01-transcode --path /mnt/pve/cyclone-01-transcode --server 192.168.1.10 --export /volume1/video/transcode --content images --options vers=4.1
 
 # Edit Proxmox host file
-echo -e "127.0.0.1 localhost.localdomain localhost
-
-# Proxmox Hosts
-192.168.1.101 typhoon-01.localdomain typhoon-01
-192.168.1.102 typhoon-02.localdomain typhoon-02
-192.168.1.103 typhoon-03.localdomain typhoon-03
-192.168.1.104 typhoon-04.localdomain typhoon-04
-
-# NAS Storage
-192.168.1.10 cyclone-01.localdomain cyclone-01
-192.168.1.11 cyclone-02.localdomain cyclone-02
-
-# Docker Nodes
-192.168.1.111 ds-01.localdomain ds-01
-192.168.1.112 ds-02.localdomain ds-02
-192.168.1.113 ds-03.localdomain ds-03
-192.168.1.114 ds-04.localdomain ds-04
-192.168.1.115 ds-05.localdomain ds-05
-192.168.1.116 ds-06.localdomain ds-06
-192.168.1.117 ds-07.localdomain ds-07
-192.168.1.118 ds-08.localdomain ds-08
-192.168.1.119 ds-09.localdomain ds-09
-
-# VM Machines
-192.168.1.253 pfsense.localdomain pfsense
-
-# LXC Machines
-192.168.1.6 unifi.localdomain unifi
-192.168.1.254 pihole.localdomain pihole
-192.168.50.20 jellyfin.localdomain jellyfin
-
-# The following lines are desirable for IPv6 capable hosts
-
-::1     ip6-localhost ip6-loopback
-fe00::0 ip6-localnet
-ff00::0 ip6-mcastprefix
-ff02::1 ip6-allnodes
-ff02::2 ip6-allrouters
-ff02::3 ip6-allhosts"  >  /etc/hosts
+hostsfile=$(wget https://raw.githubusercontent.com/ahuacate/proxmox-node/master/scripts/hosts -q -O -)
+cat << EOF > /etc/hosts
+$hostsfile
+EOF
 
 #### Here on is particular to Typhoon-01 and 6-NIC Hardware ####
 
