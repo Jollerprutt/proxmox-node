69ec9c87806de39e1ab8774f877bfc2463abace5
Ahuacate <43461046+ahuacate@users.noreply.github.com>
2020-05-17 18:41:27 +0700

    Update fileserver_setup_ct_18.04.sh

--- scripts/fileserver_setup_ct_18.04.sh
+++ scripts/fileserver_setup_ct_18.04.sh
@@ -1,8 +1,49 @@
 #!/usr/bin/env bash
 
+function info() {
+  local REASON="$1"
+  local FLAG="\e[36m[INFO]\e[39m"
+  msg "$FLAG $REASON"
+}
+function msg() {
+  local TEXT="$1"
+  echo -e "$TEXT"
+}
+function section() {
+  local REASON="  \e[97m$1\e[37m"
+  printf -- '-%.0s' {1..100}; echo ""
+  msg "$REASON"
+  printf -- '-%.0s' {1..100}; echo ""
+  echo
+}
+function box_out() {
+  set +u
+  local s=("$@") b w
+  for l in "${s[@]}"; do
+	((w<${#l})) && { b="$l"; w="${#l}"; }
+  done
+  tput setaf 3
+  echo " -${b//?/-}-
+| ${b//?/ } |"
+  for l in "${s[@]}"; do
+	printf '| %s%*s%s |\n' "$(tput setaf 7)" "-$w" "$l" "$(tput setaf 3)"
+  done
+  echo "| ${b//?/ } |
+ -${b//?/-}-"
+  tput sgr 0
+  set -u
+}
+
+# Colour
+RED=$'\033[0;31m'
+YELLOW=$'\033[1;33m'
+GREEN=$'\033[0;32m'
+WHITE=$'\033[1;37m'
+NC=$'\033[0m'
+
+
 # Download external scripts
 wget -qL https://raw.githubusercontent.com/ahuacate/proxmox-node/master/scripts/proxmox_setup_sharedfolderlist
-
 
 
 # Create users and groups
