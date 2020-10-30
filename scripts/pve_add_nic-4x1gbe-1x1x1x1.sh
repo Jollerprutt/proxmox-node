0f4eb5f4c4d1d3046471e9da482b6661e7b6df45
Ahuacate <43461046+ahuacate@users.noreply.github.com>
2020-10-30 10:59:10 +0700

    Update pve_add_nic-4x1gbe-1x1x1x1.sh

--- scripts/pve_add_nic-4x1gbe-1x1x1x1.sh
+++ scripts/pve_add_nic-4x1gbe-1x1x1x1.sh
@@ -1,4 +1,4 @@
-# Settings for default 4x NIC
+# Settings for default 4x 1Gbe
 
 # Please do NOT modify this file directly, unless you know what
 # you're doing.
@@ -13,21 +13,21 @@
 auto lo
 iface lo inet loopback
 
-iface enp${NIC_0_BUS_NO}s${NIC_0_SLOT_NO}f0 inet manual
+iface enp${NIC_0_BUS_NO}s${NIC_0_SLOT_NO}f${NIC_0_FUNCTION_NO} inet manual
 
-iface enp${NIC_1_BUS_NO}s${NIC_1_SLOT_NO}f1 inet manual
+iface enp${NIC_1_BUS_NO}s${NIC_1_SLOT_NO}f${NIC_1_FUNCTION_NO} inet manual
 
-iface enp${NIC_2_BUS_NO}s${NIC_3_SLOT_NO}f2 inet manual
+iface enp${NIC_2_BUS_NO}s${NIC_2_SLOT_NO}f${NIC_2_FUNCTION_NO} inet manual
 
-iface enp${NIC_3_BUS_NO}s${NIC_3_SLOT_NO}f3 inet manual
+iface enp${NIC_3_BUS_NO}s${NIC_3_SLOT_NO}f${NIC_3_FUNCTION_NO} inet manual
 
 #Proxmox LAN Bridge
 auto vmbr0
 iface vmbr0 inet static
         address  $PVE_HOST_IP
-        netmask  255.255.255.0
+        netmask  $PVE_NETMASK
         gateway  $PVE_GW
-        bridge-ports enp${I350_BUS_NO}s${I350_SLOT_NO}f0
+        bridge-ports enp${NIC_0_BUS_NO}s${NIC_0_SLOT_NO}f${NIC_0_FUNCTION_NO}
         bridge-stp off
         bridge-fd 0
         bridge-vlan-aware yes
@@ -36,7 +36,7 @@ iface vmbr0 inet static
 #VPN-egress Bridge
 auto vmbr1
 iface vmbr1 inet manual
-        bridge-ports enp${I350_BUS_NO}s${I350_SLOT_NO}f1
+        bridge-ports enp${NIC_1_BUS_NO}s${NIC_1_SLOT_NO}f${NIC_1_FUNCTION_NO}
         bridge-stp off
         bridge-fd 0
         bridge-vlan-aware yes
@@ -45,7 +45,7 @@ iface vmbr1 inet manual
 #vpngate-world
 auto vmbr2
 iface vmbr2 inet manual
-        bridge-ports enp${I350_BUS_NO}s${I350_SLOT_NO}f2
+        bridge-ports enp${NIC_2_BUS_NO}s${NIC_2_SLOT_NO}f${NIC_2_FUNCTION_NO}
         bridge-stp off
         bridge-fd 0
         bridge-vlan-aware yes
@@ -54,7 +54,7 @@ iface vmbr2 inet manual
 #vpngate-local
 auto vmbr3
 iface vmbr3 inet manual
-        bridge-ports enp${I350_BUS_NO}s${I350_SLOT_NO}f3
+        bridge-ports enp${NIC_3_BUS_NO}s${NIC_3_SLOT_NO}f${NIC_3_FUNCTION_NO}
         bridge-stp off
         bridge-fd 0
         bridge-vlan-aware yes
